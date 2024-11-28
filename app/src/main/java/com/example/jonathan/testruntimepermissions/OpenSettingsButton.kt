73e7c225d01468b27afde69444e17cf4bdaf9a15
Sealight Clock <chbebram@hotmail.com>
2024-11-28 00:36:11 -0800

    Added OpenSettingsButton.kt

---
+++ app/src/main/java/com/example/jonathan/testruntimepermissions/OpenSettingsButton.kt
@@ -0,0 +1,22 @@
+package com.example.jonathan.testruntimepermissions
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.core.content.ContextCompat.startActivity
+
+@Composable
+fun OpenSettingsButton(context: Context) {
+    Button(onClick = {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", "com.example.jonathan.testruntimepermissions", null)
+        }
+        context.startActivity(intent)
+    }) {
+        Text("Open App Settings")
+    }
+}
